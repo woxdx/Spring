8a2b7b10c5c15110c61c509e826cfd989bd83071
woxdx <148448845+woxdx@users.noreply.github.com>
2024-06-01 16:14:51 +0900

    06 01

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,11 +3,14 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public class MemberService { 
+
+public class MemberService {
 
     private final MemberRepository memberRepository;
 
