36ed9916d901ccf2e5ee6c25aa85695398f8f2c4
woxdx <148448845+woxdx@users.noreply.github.com>
2024-06-06 21:00:00 +0900

    06 05 commit

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -21,10 +21,12 @@ public class MemberService {
 
     // 회원 가입
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+            // 같은 이름이 있는 중복 회원 X
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -35,7 +37,9 @@ public class MemberService {
     }
     // 전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+            return memberRepository.findAll();
+
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
